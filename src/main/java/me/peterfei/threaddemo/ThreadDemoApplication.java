61f8979bfde41e44433ddbdc0aff1ccc27712a2c
peterfei <peterfeispace@gmail.com>
2022-09-27 09:15:53 +0800

    [Init]springboot 初始化

---
+++ src/main/java/me/peterfei/threaddemo/ThreadDemoApplication.java
@@ -0,0 +1,13 @@
+package me.peterfei.threaddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreadDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThreadDemoApplication.class, args);
+    }
+
+}
